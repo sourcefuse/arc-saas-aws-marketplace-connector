a51b97f7d4f3d949907796352e96bc6a5aa24bd0
Shubham P <shubham.prajapat@sourcefuse.com>
2024-09-19 13:21:36 +0530

    handle entitlement-created action to send tenant registration event

--- saas-reciever/index.js
+++ saas-reciever/index.js
@@ -1,64 +1,105 @@
 "use strict";
 const AWS = require("aws-sdk");
 const { MESSAGE_ACTION, ENV_VARS } = require("./constants");
-AWS.config.update({ region: ENV_VARS.region })
+const { PLAN_MAPPING } = require("./plan-mapping");
+AWS.config.update({ region: ENV_VARS.region });
 
 const eventBridge = new AWS.EventBridge();
 
 exports.handler = async (event) => {
-  event.Records.map(record => {
+  for (const record of event.Records) {
+    console.log("record: ", record);
     try {
       let { body } = record;
-      console.log("body", body);
+      console.log("body: ", body);
       body = JSON.parse(body);
+
       const [action, message] = body.Message.split("# ");
       const userDetails = JSON.parse(message);
-      console.log(userDetails);
-      userDetails.entitlement = typeof userDetails.entitlement == "string" ? JSON.parse(userDetails.entitlement) : userDetails.entitlement;
+      console.log("userDetails: ", userDetails);
+
+      userDetails.entitlement =
+        typeof userDetails.entitlement == "string"
+          ? JSON.parse(userDetails.entitlement)
+          : userDetails.entitlement;
+
       switch (action.toLowerCase()) {
         case MESSAGE_ACTION.ENTITLEMENT_UPDATED.toLowerCase():
-          const newPlan = userDetails.entitlement.Entitlements[0]["Dimension"];
-          console.log("Customer Choose new plan:-", newPlan.split("_")[0]);
+          break;
+        case MESSAGE_ACTION.ENTITLEMENT_CREATED.toLowerCase():
+          const selectedPlan =
+            userDetails.entitlement.Entitlements[0]["Dimension"];
+          const marketplacePlanKey = selectedPlan.split("_")[0];
+          console.log("Choosen plan:-", marketplacePlanKey);
+          const controlPlanePlanIdentifier = PLAN_MAPPING[marketplacePlanKey];
+
+          if (!controlPlanePlanIdentifier) {
+            console.error(
+              "Corresponding Plan not found in the mapping object."
+            );
+            break;
+          }
           const tenantRegistrationData = {
-            "customer": {
-              "firstName": userDetails.firstName,
-              "lastName": userDetails.lastName,
-              "email": userDetails.email,
-              "address": userDetails.address,
-              "zip": userDetails.zipcode,
-              "country": userDetails.country
+            company: {
+              name: userDetails.companyName,
             },
-            "appConfig": {
-              "preferredSubdomain": userDetails.preferredSubdomain
+            customer: {
+              firstName: userDetails.firstName,
+              lastName: userDetails.lastName,
+              email: userDetails.email,
+              address: userDetails.address,
+              zip: userDetails.zipcode,
+              country: userDetails.country,
             },
-            "plan": {
-              "identifier": newPlan
-            }
+            appConfig: {
+              preferredSubdomain: userDetails.preferredSubdomain,
+            },
+            plan: {
+              identifier: controlPlanePlanIdentifier,
+            },
           };
+          console.log("tenantRegistrationData", tenantRegistrationData);
           // Event parameters
-          const params = {
+          const tenantRegistrationEventParams = {
             Entries: [
               {
-                Source: 'com.saas.marketplace',
-                DetailType: 'TENANT_REGISTRATION',
+                Source: "com.saas.marketplace",
+                DetailType: "TENANT_REGISTRATION",
                 EventBusName: ENV_VARS.EventBusName,
                 Time: new Date(),
-                Detail: JSON.stringify(tenantRegistrationData)
-              }
-            ]
+                Detail: JSON.stringify(tenantRegistrationData),
+              },
+            ],
           };
-          console.log(params);
-          break;
-        case MESSAGE_ACTION.SUBSCRIBE_SUCCESS.toLowerCase():
+          console.log(
+            "tenantRegistrationEventParams",
+            tenantRegistrationEventParams
+          );
+          // send event to event bridge
+          const registrationEvent = await eventBridge
+            .putEvents({
+              Entries: tenantRegistrationEventParams.Entries,
+            })
+            .promise()
+            .catch((err) => {
+              console.log(
+                "Error in sending registration event to event bridge",
+                err
+              );
+            });
+
+          console.log("registrationEvent sent", registrationEvent);
           break;
         case MESSAGE_ACTION.UNSUBSCRIBE_SUCCESS.toLowerCase():
           break;
       }
     } catch (e) {
-      console.log(e.message);
+      console.log(
+        "Something went wrong while processing the record:",
+        e.message
+      );
     }
-  });
-  return {}
-}
+  }
+};
 
 // exports.handler(require("./events/sqs_event.json"));
